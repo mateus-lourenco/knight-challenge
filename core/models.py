f9e22d567eda197298c51acded704edb00618c46
Mateus Lourenço <mateus.c.lourenco@accenture.com>
2021-01-22 15:58:40 -0300

    add piece's dao and endpoint

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Piece(models.Model):
+
+    TYPE_CHOICES = (('kg', 'king'),
+                    ('qn', 'queen'),
+                    ('rk', 'rook'),
+                    ('bp', 'bishop'),
+                    ('kt', 'knight'),
+                    ('pn', 'pawn'),
+                    )
+    COLOR_CHOICE = (('bk', 'black'), ('wt', 'white'))
+
+    name = models.CharField(_("piece's name/type"),
+                            max_length=50,
+                            choices=TYPE_CHOICES)
+
+    color = models.CharField(_("piece's color"),
+                             max_length=50,
+                             choices=COLOR_CHOICE)
+
+    class Meta:
+        db_table = "piece"
